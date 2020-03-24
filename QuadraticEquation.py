01afba2e96a34c277730bff59c03540a250f7c62
Daryana-2906 <58994456+Daryana-2906@users.noreply.github.com>
2020-03-24 21:38:45 +0300

    Create QuadraticEquation.py

---
+++ QuadraticEquation.py
@@ -0,0 +1,16 @@
+a = int(input('введите коэффициент перед х^2: '))
+b = int(input('введите коэффициент перед х: '))
+c = int(input('введите свободный член: '))
+
+def sol_of_guadratic_equation(a, b, c):
+    import math
+    try:
+        root1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+        root2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+        if root1 != root2:
+            return root1, root2
+        else:
+            return root1
+    except ValueError:
+        return 'корней нет'
+print(sol_of_guadratic_equation(a, b, c)
